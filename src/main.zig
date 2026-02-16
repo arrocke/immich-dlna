3fb7cc586637aac14dfb1dae1c790724af49763f
Adrian Rocke <me@adrianrocke.com>
2026-02-15 23:24:38 -0600

    feat: structs for building browse responses

--- src/main.zig
+++ src/main.zig
@@ -41,72 +41,18 @@ fn upnpCallback(
                     const objectId = std.mem.span(c.ixmlNode_getNodeValue(c.ixmlNode_getFirstChild(node)));
                     std.log.info("ObjectId: {s}", .{objectId});
 
-                    const doc = c.ixmlDocument_createDocument();
-                    const root = c.ixmlDocument_createElementNS(
-                        doc,
-                        "urn:schemas-upnp-org:service:ContentDirectory:1",
-                        "u:BrowseResponse",
-                    );
-                    _ = c.ixmlElement_setAttributeNS(
-                        root,
-                        "urn:schemas-upnp-org:service:ContentDirectory:1",
-                        "xmlns:u",
-                        "urn:schemas-upnp-org:service:ContentDirectory:1",
-                    );
-                    _ = c.ixmlNode_appendChild(@ptrCast(doc), @ptrCast(root));
-
-                    const didl_doc = c.ixmlDocument_createDocument();
-                    const didl_root = c.ixmlDocument_createElementNS(
-                        didl_doc,
-                        "urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/",
-                        "DIDL-Lite",
-                    );
-                    _ = c.ixmlElement_setAttribute(
-                        didl_root,
-                        "xmlns:dc",
-                        "http://purl.org/dc/elements/1.1/",
-                    );
-                    _ = c.ixmlElement_setAttribute(
-                        didl_root,
-                        "xmlns:upnp",
-                        "urn:schemas-upnp-org:metadata-1-0/upnp/",
-                    );
-                    _ = c.ixmlNode_appendChild(@ptrCast(didl_doc), @ptrCast(didl_root));
-
-                    const container = c.ixmlDocument_createElement(didl_doc, "container");
-                    _ = c.ixmlElement_setAttribute(container, "id", "1");
-                    _ = c.ixmlElement_setAttribute(container, "parentID", "0");
-                    _ = c.ixmlElement_setAttribute(container, "restricted", "1");
-
-                    const dc_title = c.ixmlDocument_createElementNS(
-                        didl_doc,
-                        "http://purl.org/dc/elements/1.1/",
-                        "dc:title",
-                    );
-                    const title_text = c.ixmlDocument_createTextNode(didl_doc, "Albums");
-                    _ = c.ixmlNode_appendChild(@ptrCast(dc_title), @ptrCast(title_text));
-                    _ = c.ixmlNode_appendChild(@ptrCast(container), @ptrCast(dc_title));
-
-                    const upnp_class = c.ixmlDocument_createElementNS(
-                        didl_doc,
-                        "urn:schemas-upnp-org:metadata-1-0/upnp/",
-                        "upnp:class",
-                    );
-                    const class_text = c.ixmlDocument_createTextNode(didl_doc, "object.container");
-                    _ = c.ixmlNode_appendChild(@ptrCast(upnp_class), @ptrCast(class_text));
-                    _ = c.ixmlNode_appendChild(@ptrCast(container), @ptrCast(upnp_class));
-
-                    _ = c.ixmlNode_appendChild(@ptrCast(didl_root), @ptrCast(container));
-
-                    const didl_string = c.ixmlDocumenttoString(didl_doc);
-                    defer c.ixmlFreeDOMString(didl_string);
-
-                    add_element(doc, root, "Result", didl_string);
-                    add_element(doc, root, "NumberReturned", "1");
-                    add_element(doc, root, "TotalMatches", "1");
-                    add_element(doc, root, "UpdateID", "1");
+                    const response = BrowseReponse{
+                        .albums = &[_]Album{.{ .id = "1", .parentId = "0", .name = "Test Album" }},
+                        .totalMatches = "1",
+                        .numberReturned = "1",
+                        .updateId = "1",
+                    };
 
                     const mutableRequest: *c.UpnpActionRequest = @constCast(request);
+                    const doc = response.toIXMLDocument();
+
+                    std.log.info("Response: {s}", .{c.ixmlDocumenttoString(doc)});
+
                     _ = c.UpnpActionRequest_set_ActionResult(mutableRequest, doc);
                 }
             }
@@ -118,6 +64,102 @@ fn upnpCallback(
 
     return 0;
 }
+
+const BrowseReponse = struct {
+    albums: []const Album,
+    updateId: [:0]const u8,
+    numberReturned: [:0]const u8,
+    totalMatches: [:0]const u8,
+
+    pub fn toIXMLDocument(self: *const BrowseReponse) [*c]c.struct__IXML_Document {
+        const didlDocument = c.ixmlDocument_createDocument();
+        const didlElement = c.ixmlDocument_createElement(didlDocument, "DIDL-Lite");
+        _ = c.ixmlElement_setAttribute(didlElement, "xmlns:dc", "http://purl.org/dc/elements/1.1/");
+        _ = c.ixmlElement_setAttribute(didlElement, "xmlns:upnp", "urn:schemas-upnp-org:metadata-1-0/upnp/");
+        _ = c.ixmlNode_appendChild(@ptrCast(didlDocument), @ptrCast(didlElement));
+
+        for (self.albums) |album| {
+            const albumElement = album.toIXMLElement(@ptrCast(didlDocument));
+            _ = c.ixmlNode_appendChild(@ptrCast(didlElement), @ptrCast(albumElement));
+        }
+
+        const didlStr = c.ixmlDocumenttoString(didlDocument);
+
+        const document = c.ixmlDocument_createDocument();
+        const browseResponseElement = c.ixmlDocument_createElement(document, "u:BrowseResponse");
+        _ = c.ixmlElement_setAttribute(browseResponseElement, "xmlns:u", "urn:schemas-upnp-org:service:ContentDirectory:1");
+        _ = c.ixmlNode_appendChild(@ptrCast(document), @ptrCast(browseResponseElement));
+
+        {
+            const element = c.ixmlDocument_createElement(document, "Result");
+            const text = c.ixmlDocument_createTextNode(document, didlStr);
+            _ = c.ixmlNode_appendChild(@ptrCast(element), @ptrCast(text));
+            _ = c.ixmlNode_appendChild(@ptrCast(browseResponseElement), @ptrCast(element));
+        }
+
+        {
+            const element = c.ixmlDocument_createElement(document, "NumberReturned");
+            const text = c.ixmlDocument_createTextNode(document, self.numberReturned);
+            _ = c.ixmlNode_appendChild(@ptrCast(element), @ptrCast(text));
+            _ = c.ixmlNode_appendChild(@ptrCast(browseResponseElement), @ptrCast(element));
+        }
+
+        {
+            const element = c.ixmlDocument_createElement(document, "TotalMatches");
+            const text = c.ixmlDocument_createTextNode(document, self.totalMatches);
+            _ = c.ixmlNode_appendChild(@ptrCast(element), @ptrCast(text));
+            _ = c.ixmlNode_appendChild(@ptrCast(browseResponseElement), @ptrCast(element));
+        }
+
+        {
+            const element = c.ixmlDocument_createElement(document, "UpdateId");
+            const text = c.ixmlDocument_createTextNode(document, self.updateId);
+            _ = c.ixmlNode_appendChild(@ptrCast(element), @ptrCast(text));
+            _ = c.ixmlNode_appendChild(@ptrCast(browseResponseElement), @ptrCast(element));
+        }
+
+        return document;
+    }
+};
+
+const Album = struct {
+    id: [:0]const u8,
+    parentId: [:0]const u8,
+    name: [:0]const u8,
+
+    pub fn toIXMLElement(self: *const Album, document: *c.struct__IXML_Document) [*c]c.struct__IXML_Element {
+        const albumElement = c.ixmlDocument_createElement(document, "container");
+        _ = c.ixmlElement_setAttribute(albumElement, "id", self.id);
+        _ = c.ixmlElement_setAttribute(albumElement, "parentID", self.parentId);
+        _ = c.ixmlElement_setAttribute(albumElement, "restricted", "1");
+
+        const titleElement = c.ixmlDocument_createElement(document, "dc:title");
+        const titleText = c.ixmlDocument_createTextNode(document, self.name);
+        _ = c.ixmlNode_appendChild(@ptrCast(titleElement), @ptrCast(titleText));
+        _ = c.ixmlNode_appendChild(@ptrCast(albumElement), @ptrCast(titleElement));
+
+        const classElement = c.ixmlDocument_createElement(document, "upnp:class");
+        const classText = c.ixmlDocument_createTextNode(document, "object.container");
+        _ = c.ixmlNode_appendChild(@ptrCast(classElement), @ptrCast(classText));
+        _ = c.ixmlNode_appendChild(@ptrCast(albumElement), @ptrCast(classElement));
+
+        return albumElement;
+    }
+};
+
+const IXMLAttribute = struct {
+    name: [:0]const u8,
+    value: [:0]const u8,
+};
+
+const IXMLElement = union(enum) {
+    element: struct {
+        name: [:0]const u8,
+        attributes: []const IXMLAttribute,
+        children: []const IXMLElement,
+    },
+    text: [:0]const u8,
+};
 
 fn add_element(doc: *c.IXML_Document, parent: *c.IXML_Element, name: [*c]const u8, value: [*c]const u8) void {
     const el = c.ixmlDocument_createElement(doc, name);
