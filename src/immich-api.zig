ef98b639c732dcfb7adc46305290293cec5e0072
Adrian Rocke <me@adrianrocke.com>
2026-02-17 16:29:47 -0600

    refactor: simplify immich api and improve logging

--- src/immich-api.zig
+++ src/immich-api.zig
@@ -2,102 +2,6 @@ const std = @import("std");
 
 const Self = @This();
 
-apiKey: []const u8,
-baseUrl: []const u8,
-allocator: std.mem.Allocator,
-client: std.http.Client,
-
-pub fn init(allocator: std.mem.Allocator, apiKey: []const u8, baseUrl: []const u8) Self {
-    return .{
-        .apiKey = apiKey,
-        .baseUrl = baseUrl,
-        .allocator = allocator,
-        .client = std.http.Client{ .allocator = allocator },
-    };
-}
-
-pub fn deinit(self: *Self) void {
-    self.client.deinit();
-}
-
-pub fn getAlbums(self: *Self) !std.json.Parsed([]Album) {
-    var body = std.Io.Writer.Allocating.init(self.allocator);
-    defer body.deinit();
-
-    const url = try std.fmt.allocPrint(self.allocator, "{s}/albums", .{self.baseUrl});
-    defer self.allocator.free(url);
-
-    const response = try self.client.fetch(.{
-        .location = .{ .url = url },
-        .method = .GET,
-        .extra_headers = &[_]std.http.Header{
-            .{ .name = "x-api-key", .value = self.apiKey },
-        },
-        .response_writer = &body.writer,
-    });
-
-    if (response.status != .ok) {
-        std.log.err("Request failed with status: {}\n", .{response.status});
-        return error.HttpRequestFailed;
-    }
-
-    const albums: std.json.Parsed([]Album) = try std.json.parseFromSlice([]Album, self.allocator, body.written(), .{ .ignore_unknown_fields = true });
-
-    return albums;
-}
-
-pub fn getAlbum(self: *Self, id: []const u8) !std.json.Parsed(Album) {
-    var body = std.Io.Writer.Allocating.init(self.allocator);
-    defer body.deinit();
-
-    const url = try std.fmt.allocPrint(self.allocator, "{s}/albums/{s}", .{ self.baseUrl, id });
-    defer self.allocator.free(url);
-
-    const response = try self.client.fetch(.{
-        .location = .{ .url = url },
-        .method = .GET,
-        .extra_headers = &[_]std.http.Header{
-            .{ .name = "x-api-key", .value = self.apiKey },
-        },
-        .response_writer = &body.writer,
-    });
-
-    if (response.status != .ok) {
-        std.log.err("Request failed with status: {}\n", .{response.status});
-        return error.HttpRequestFailed;
-    }
-
-    const album: std.json.Parsed(Album) = try std.json.parseFromSlice(Album, self.allocator, body.written(), .{ .ignore_unknown_fields = true });
-
-    return album;
-}
-
-pub fn getAsset(self: *Self, id: []const u8) !std.json.Parsed(Asset) {
-    var body = std.Io.Writer.Allocating.init(self.allocator);
-    defer body.deinit();
-
-    const url = try std.fmt.allocPrint(self.allocator, "{s}/assets/{s}", .{ self.baseUrl, id });
-    defer self.allocator.free(url);
-
-    const response = try self.client.fetch(.{
-        .location = .{ .url = url },
-        .method = .GET,
-        .extra_headers = &[_]std.http.Header{
-            .{ .name = "x-api-key", .value = self.apiKey },
-        },
-        .response_writer = &body.writer,
-    });
-
-    if (response.status != .ok) {
-        std.log.err("Request failed with status: {}\n", .{response.status});
-        return error.HttpRequestFailed;
-    }
-
-    const asset: std.json.Parsed(Asset) = try std.json.parseFromSlice(Asset, self.allocator, body.written(), .{ .ignore_unknown_fields = true });
-
-    return asset;
-}
-
 pub const Album = struct {
     id: []u8,
     albumName: []u8,
@@ -118,3 +22,90 @@ pub const Asset = struct {
     exifInfo: EixfInfo,
     updatedAt: []u8,
 };
+
+apiKey: []const u8,
+baseUrl: []const u8,
+allocator: std.mem.Allocator,
+client: std.http.Client,
+
+pub fn init(allocator: std.mem.Allocator, apiKey: []const u8, baseUrl: []const u8) Self {
+    return .{
+        .apiKey = apiKey,
+        .baseUrl = baseUrl,
+        .allocator = allocator,
+        .client = std.http.Client{ .allocator = allocator },
+    };
+}
+
+pub fn deinit(self: *Self) void {
+    self.client.deinit();
+}
+
+const GetRequestError = error{
+    HttpRequestFailed,
+    OutOfMemory,
+    FailedStatusCode,
+};
+
+pub fn get(self: *Self, Response: type, comptime path: []const u8, pathArgs: anytype) GetRequestError!std.json.Parsed(Response) {
+    std.log.debug("[ImmichApi.get] Starting request for path: {s}", .{path});
+
+    const fullPath = std.fmt.allocPrint(self.allocator, path, pathArgs) catch |err| {
+        std.log.err("[ImmichApi.get] Failed to format URL path: {}", .{err});
+        return err;
+    };
+    defer self.allocator.free(fullPath);
+
+    const url = std.fmt.allocPrint(self.allocator, "{s}{s}", .{ self.baseUrl, fullPath }) catch |err| {
+        std.log.err("[ImmichApi.get] Failed to format URL: {}", .{err});
+        return err;
+    };
+    defer self.allocator.free(url);
+
+    var body = std.Io.Writer.Allocating.init(self.allocator);
+    defer body.deinit();
+
+    std.log.info("[ImmichApi.get] {s}", .{url});
+
+    const response = self.client.fetch(.{
+        .location = .{ .url = url },
+        .method = .GET,
+        .extra_headers = &[_]std.http.Header{
+            .{ .name = "x-api-key", .value = self.apiKey },
+        },
+        .response_writer = &body.writer,
+    }) catch |err| {
+        std.log.err("[ImmichApi.get] Get request failed: {}", .{err});
+        return error.HttpRequestFailed;
+    };
+    if (response.status != .ok) {
+        std.log.err("[ImmichApi.get] Request failed with status: {}", .{response.status});
+        return error.FailedStatusCode;
+    }
+
+    const result = std.json.parseFromSlice(
+        Response,
+        self.allocator,
+        body.written(),
+        .{ .ignore_unknown_fields = true },
+    ) catch |err| {
+        std.log.err("[ImmichApi.get] Failed to parse response: {}", .{err});
+        return error.HttpRequestFailed;
+    };
+
+    std.log.debug("[ImmichApi.get] Request succeeded", .{});
+
+    return result;
+}
+
+pub fn getAlbums(self: *Self) !std.json.Parsed([]Album) {
+    return self.get([]Album, "/albums", .{});
+}
+
+pub fn getAlbum(self: *Self, id: []const u8) !std.json.Parsed(Album) {
+    return self.get(Album, "/albums/{s}", .{id});
+}
+
+pub fn getAsset(self: *Self, id: []const u8) !std.json.Parsed(Asset) {
+    return self.get(Asset, "/assets/{s}", .{id});
+}
