c8552b0d96bf0fdd640e54870ec356958c4f1a8c
Adrian Rocke <me@adrianrocke.com>
2026-02-18 20:56:39 -0600

    fix: nix package source root

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
         pname = "immich-dlna";
         version = "0.1.0";
 
-        src = ./src;
+        src = ./.;
 
         nativeBuildInputs = [ zigPkg pkgs.pkg-config ];
         buildInputs = [ pkgs.libupnp ];
